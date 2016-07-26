03277ee11e64096ee6ec3359ddbbec9b9955200a
fanai <741250961@qq.com>
2016-07-26 11:28:25 +0800

    add vimrc_conf file

---
+++ vimrc_conf/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/CandidateRepository_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/CandidateRepository_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/Candidate_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/Candidate_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/ClangCompleter/ClangCompleter_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/ClangCompleter/ClangCompleter_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/ClangCompleter/TranslationUnit_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/ClangCompleter/TranslationUnit_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/IdentifierCompleter_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/IdentifierCompleter_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/IdentifierUtils_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/IdentifierUtils_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/IndexForLetter_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/IndexForLetter_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/IsUppercase_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/IsUppercase_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/LetterBitsetFromString_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/LetterBitsetFromString_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/LetterNode_test.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/LetterNode_test.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/TestUtils.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/TestUtils.cpp.o"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/main.cpp" "/home/fanai/ycm_build/ycm/tests/CMakeFiles/ycm_core_tests.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "BOOST_THREAD_DONT_USE_CHRONO"
+  "USE_CLANG_COMPLETER"
+  "YCMD_CORE_VERSION=24"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/fanai/ycm_build/BoostParts/CMakeFiles/BoostParts.dir/DependInfo.cmake"
+  "/home/fanai/ycm_build/ycm/CMakeFiles/ycm_core.dir/DependInfo.cmake"
+  "/home/fanai/ycm_build/ycm/tests/gmock/CMakeFiles/gmock.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/ClangCompleter"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/BoostParts"
+  "/usr/include/python2.7"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/llvm/include"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/gmock"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/gmock/include"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/gmock/gtest"
+  "/home/fanai/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/tests/gmock/gtest/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
